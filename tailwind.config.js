abe2153e5108a2d1f0f0ef4700054294a7564d23
Thomas Ellwood <taellwood@gmail.com>
2024-05-22 11:44:02 +0100

    addedd Tailwind configuartion

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",y
+  ],
+  // Set custom settings below
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1DA1F2',
+        secondary: '#14171A',
+        accent: '#657786',
+        background: '#F5F8FA',
+      },
+    },
+  },
+  // Include any plugins to use below
+  plugins: [],
+}
