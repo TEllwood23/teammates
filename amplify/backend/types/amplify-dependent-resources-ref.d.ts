e65e6fd8d612c2d94c402a142965752b736515b2
Thomas Ellwood <taellwood@gmail.com>
2024-05-22 16:40:59 +0100

    updated GrapQL schema to reflect depreciation of '@connected'

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "teammates": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "teammatesbeed158c": {
       "AppClientID": "string",
