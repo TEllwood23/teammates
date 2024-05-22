80a64d3639f6915271ff3ed9304809e147245093
Thomas Ellwood <taellwood@gmail.com>
2024-05-22 14:51:14 +0100

    fixed issue with Amplify config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import {Amplify} from 'aws-amplify';
+import config from '../aws-exports'
+
+Amplify.configure(config);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
